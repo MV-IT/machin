de9efe1708c18eb5d2746ae6383838729fa6dc25
Lai Ba Vi <017laibavi.wts@gmail.com>
2017-10-08 22:26:18 +0700

    Add profile.php

--- views/user/profile.php
+++ views/user/profile.php
@@ -1,2 +1,75 @@
 <?php require 'views/header.php'; ?>
+<div class="container">
+	<div class="row col-content">
+		<div class="content-profile col-md-12 col-12">
+			<div class="row col-content">
+				<div class="col-lg-3 col-md-4 col-4 img-profile">
+					<img src="https://scontent.fhan2-3.fna.fbcdn.net/v/t1.0-9/20767768_905359746281743_1352464433750941234_n.jpg?oh=c7f1ac12102b9fe1b75837736c253144&oe=5A85A910" alt="">
+				</div>
+				<div class="profile-person col-lg-9 col-md-8 col-12">
+					<div class="row  group">
+						<div class="col-lg-2 col-md-4 col-5 box-title">
+							<p class="title-info">Họ tên <span class="haicham">:</span> </p>
+						</div>
+						<div class="col-lg-9 col-md-7 col-6 box-content">
+							<p class="content-info">Lai Bá Vĩ</p>
+						</div>
+						<div class="col-1"></div>
+					</div>
+					<div class="row  group">
+						<div class="col-lg-2 col-md-4 col-5 box-title">
+							<p class="title-info">Giới tính <span class="haicham">:</span></p>
+						</div>
+						<div class="col-lg-9 col-md-7 col-6 box-content">
+							<p class="content-info">Nam</p>
+						</div>
+						<div class="col-1"></div>
+					</div>
+					<div class="row  group">
+						<div class="col-lg-2 col-md-4 col-5 box-title">
+							<p class="title-info">Ngày sinh <span class="haicham">:</span></p>
+						</div>
+						<div class="col-lg-9 col-md-7 col-6 box-content">
+							<p class="content-info">7/10/1998</p>
+						</div>
+						<div class="col-1"></div>
+					</div>
+					<div class="row group">
+						<div class="col-lg-2 col-md-4 col-5 box-title">
+							<p class="title-info">Số điện thoại <span class="haicham">:</span></p>
+						</div>
+
+						<div class="col-lg-9 col-md-7 col-6 box-content">
+							<p class="content-info">01637520011</p>
+						</div>
+						<div class="col-1"></div>
+					</div>
+					<div class="row group">
+						<div class="col-lg-2 col-md-4 col-5 box-title">
+							<p class="title-info">Địa chỉ <span class="haicham">:</span></p>
+						</div>
+
+						<div class="col-lg-9 col-md-7 col-6 box-content">
+							<p class="content-info">Số 54 ngõ 125 Nguyễn Ngọc Vũ, phường Trung Hòa, Cầu Giấy Hà Nội.</p>
+						</div>
+						<div class="col-1"></div>
+					</div>
+					<div class="row group">
+						<div class="col-lg-2 col-md-4 col-5 box-title">
+							<p class="title-info">Email <span class="haicham">:</span></p>
+						</div>
+
+						<div class="col-lg-9 col-md-7 col-6 box-content">
+							<p class="content-info"><a href="https://gmail.com">017laibavi.wts@gmail.com</a></p>
+						</div>
+						<div class="col-1"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-12 col-12 list-bill">
+			
+		</div>
+	</div>
+</div>
 <?php require 'views/footer.php'; ?>
